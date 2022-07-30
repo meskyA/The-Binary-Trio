89ac124bd6a97cca7e1adab8af8708a2b2bdc02d
Jose San Mateo <info@joseasanmateo.com>
2022-07-29 20:09:49 -0700

    Changes from Bottom

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fde3feb0d9mshfba6ebe84fec58dp1d0f19jsnc5c273e3a60a',
-		'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
-	}
-};
+var tickerBox = document.createElement("p");
+tickerBox.textContent = "Nostrud velit eu sunt labore aute velit dolore ea deserunt quis magna minim proident.";
 
-fetch('https://sportscore1.p.rapidapi.com/sports', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+tickerComplete = document.getElementById("sportsTicker");
+tickerComplete.appendChild(tickerBox);
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'fde3feb0d9mshfba6ebe84fec58dp1d0f19jsnc5c273e3a60a',
+// 		'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://sportscore1.p.rapidapi.com/sports', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
