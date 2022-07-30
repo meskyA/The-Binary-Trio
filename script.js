c146872a22621655cd06c817a8922b018f0853c6
meskyA <mesky2015@gmail.com>
2022-07-29 20:02:30 -0700

    modiried css to make navbar persistent and js forfunctional navbar

--- script.js
+++ script.js
@@ -10,4 +10,22 @@ fetch('https://sportscore1.p.rapidapi.com/sports', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
-	
+
+// Nav bar targeting containers of the different sports
+const navBar = document.querySelector("#navbar")
+navBar.addEventListener('click', ()=> {
+    navBar.classList.toggle("is-active")
+})
+function toggle() {
+    const navBar = document.querySelector("#navbar")
+	if ($navBar.length > 0) {
+		$navBar.forEach(function($el){
+			$el.addEventListener('click', function(){
+				var target = $el.dataset.target;
+				var $target = document.getElementById(target);
+			})
+		})
+	}  
+    myNav.classList.toggle("is-active")
+
+}
